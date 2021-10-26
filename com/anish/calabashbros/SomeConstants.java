3f2270e2cc256e8a83f2c9c6ee5295dafd3b42c4
Kisekis <80084060+Kisekis@users.noreply.github.com>
2021-10-26 16:39:34 +0800

    Add files via upload

--- com/anish/calabashbros/SomeConstants.java
+++ com/anish/calabashbros/SomeConstants.java
@@ -7,9 +7,9 @@ public interface SomeConstants {
     static final int HEIGHT = 50;
     static final int START_X = (WIDTH-mazeNum)>>1;
     static final int START_Y = (HEIGHT-mazeNum)>>1;
-    static final int END_X = (WIDTH-mazeNum)>>1+mazeNum-1;
-    static final int END_Y = (HEIGHT-mazeNum)>>1+mazeNum-1;
-    static final int COUNT_X = (WIDTH-mazeNum)>>1 + mazeNum;
+    static final int END_X = WIDTH-(WIDTH-mazeNum)/2-1;
+    static final int END_Y = HEIGHT-(WIDTH-mazeNum)/2-1;
+    static final int COUNT_X = WIDTH/2;
     static final int COUNT_Y = (WIDTH-mazeNum)>>2;
 
 }
